2751eff5c85209181c18e526698ffa9aa9d56427
unknown <mahadyhasankbc710@gmail.com>
2018-10-05 17:41:38 +0600

    Create a  class ShapeCalc Which extends AFrame

---
+++ src/shapeCalc/ShapeCalc.java
@@ -0,0 +1,15 @@
+package shapeCalc;
+
+public  class ShapeCalc extends AFrame {
+
+	public ShapeCalc(String title) {
+		super(title);
+		
+	}
+
+	@Override
+	protected void initialize() {
+		
+	}
+
+}
