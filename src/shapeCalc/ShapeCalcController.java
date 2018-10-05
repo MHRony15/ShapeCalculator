5c0afb279a9dd4a74f7f3ff8048ea837a2df7ddb
unknown <mahadyhasankbc710@gmail.com>
2018-10-05 17:16:08 +0600

    Run Document's Main Method

---
+++ src/shapeCalc/ShapeCalcController.java
@@ -0,0 +1,17 @@
+package shapeCalc;
+
+public class ShapeCalcController {
+	public static void ShapeCalcController() {
+		System.out.println("Hellow");
+		
+		
+	}
+	
+	public static void main(String[] arg) {
+		ShapeCalcController();
+	}
+
+	
+	
+}
+
